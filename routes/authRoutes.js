7b5f2ab5dd65fe41679288b254dc1018e2dc1f48
it21296796 <99311998+it21296796@users.noreply.github.com>
2025-04-02 00:42:01 +0530

    sks

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+//dkdj
 
 const router = express.Router();
 const SECRET_KEY = process.env.JWT_SECRET || "supersecretkey";
